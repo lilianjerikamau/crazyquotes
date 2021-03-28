c65311071e418cbeb4a03d43316b56d9c53765e3
lilian <you@example.com>
2021-03-28 23:03:56 +0300

    highlight directive

--- src/app/the-quote/the-quote.component.ts
+++ src/app/the-quote/the-quote.component.ts
@@ -16,7 +16,7 @@ export class TheQuoteComponent implements OnInit {
     new Quote (3,'Mikey','Life','The way to get started is to quit talking and begin doing..','Walt Disney',new Date(2019,6,3),0,0),
     new Quote (4,'Beatrice','Communication','If life were predictable it would cease to be life, and be without flavor..','Eleanor Roosevelt',new Date(2019,7,7),0,0),
     new Quote (5,'Charles','Dear self','If you set your goals ridiculously high and its a failure, you will fail above everyone elses success..','Harold Abelson',new Date(2019,7,17),0,0),
-    new Quote (6,'Jaycee','Tech and life','One day at a timegit.','Alice Kahn',new Date(2019,7,15),0,0)
+    new Quote (6,'Jaycee','Tech and life','"Life is what happens when you are busy making other plans.','John Lennon',new Date(2019,7,15),0,0)
 
    ];
   
