9967ccfbc2ed6dda519e8f32711055909e87efc6
lilian <you@example.com>
2021-03-28 22:59:14 +0300

    nelson

--- src/app/the-quote/the-quote.component.ts
+++ src/app/the-quote/the-quote.component.ts
@@ -12,7 +12,7 @@ export class TheQuoteComponent implements OnInit {
   title = "welcome to crazy quotes";
    quotes:Quote[] = [
     new Quote (1,'Lilian','Progress','You have to write the book that wants to be written. And if the book will be too difficult for grown-ups, then you write it for children.','Madeleine LEngle', new Date(2019,7,12),0,0),
-    new Quote (2,'Wendy','Personality','Be the person yor dog thinks you are.','Max Frisch',new Date(2019,7,14),0,0),
+    new Quote (2,'Wendy','Personality','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela',new Date(2019,7,14),0,0),
     new Quote (3,'Mikey','Life','Everything is going to be alright.','Libby Larsen',new Date(2019,6,3),0,0),
     new Quote (4,'Beatrice','Communication','Be loyal to your future not your past.','Clay Shirky',new Date(2019,7,7),0,0),
     new Quote (5,'Charles','Dear self','Less fear more hope.','Harold Abelson',new Date(2019,7,17),0,0),
