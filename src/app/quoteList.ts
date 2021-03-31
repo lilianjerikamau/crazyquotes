28f0024434765e1eeba1db0b3c50be33d0451854
lilian <you@example.com>
2021-03-31 10:29:01 +0300

    datepipe

--- src/app/quoteList.ts
+++ src/app/quoteList.ts
@@ -1,11 +1,11 @@
 import { Quote } from './quote';
 
 export let Quotes: Quote[] = [
-    new Quote(1, 'Lilian', 'Progress', 'You have to write the book that wants to be written. And if the book will be too difficult for grown-ups, then you write it for children.', 'Madeleine LEngle', new Date(2019, 7, 12), 0, 0),
-    new Quote(2, 'Wendy', 'Personality', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date(2019, 7, 14), 0, 0),
-    new Quote(3, 'Mikey', 'Life', 'The way to get started is to quit talking and begin doing..', 'Walt Disney', new Date(2019, 6, 3), 0, 0),
-    new Quote(4, 'Beatrice', 'Communication', 'If life were predictable it would cease to be life, and be without flavor..', 'Eleanor Roosevelt', new Date(2019, 7, 7), 0, 0),
-    new Quote(5, 'Charles', 'Dear self', 'If you set your goals ridiculously high and its a failure, you will fail above everyone elses success..', 'Harold Abelson', new Date(2019, 7, 17), 0, 0),
-    new Quote(6, 'Jaycee', 'Tech and life', '"Life is what happens when you are busy making other plans.', 'John Lennon', new Date(2019, 7, 15), 0, 0)
+    new Quote(1, 'Lilian', 'Progress', 'You have to write the book that wants to be written. And if the book will be too difficult for grown-ups, then you write it for children.', 'Madeleine LEngle', new Date(2022, 7, 12), 0, 0),
+    new Quote(2, 'Wendy', 'Personality', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date(2022, 7, 14), 0, 0),
+    new Quote(3, 'Mikey', 'Life', 'The way to get started is to quit talking and begin doing..', 'Walt Disney', new Date(2022, 6, 3), 0, 0),
+    new Quote(4, 'Beatrice', 'Communication', 'If life were predictable it would cease to be life, and be without flavor..', 'Eleanor Roosevelt', new Date(2022, 7, 7), 0, 0),
+    new Quote(5, 'Charles', 'Dear self', 'If you set your goals ridiculously high and its a failure, you will fail above everyone elses success..', 'Harold Abelson', new Date(2022, 7, 17), 0, 0),
+    new Quote(6, 'Jaycee', 'Tech and life', '"Life is what happens when you are busy making other plans.', 'John Lennon', new Date(2022, 7, 15), 0, 0)
 
 ]
